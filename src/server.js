cfbbd3025f05056f9d32347a05db0fff5269f28a
AveryOn <lnkonion2@gmail.com>
2024-09-18 23:51:31 +0300

    fix

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const path = require('path');
 const { initProjects, startProjectServer } = require('./utils/projects');
-const PUBLIC_DIR = path.join(__dirname, 'projects');
+const SOURCE_DIR_NAME = path.join(__dirname, process.env.SOURCE_DIR_NAME || 'projects');
 
 
 (async () => {
-    const { projects } = await initProjects(PUBLIC_DIR);
+    const { projects } = await initProjects(SOURCE_DIR_NAME);
     if(Array.isArray(projects)) {
         projects.forEach((project) => {
             if(project.ownerPath) {
@@ -16,7 +16,7 @@ const PUBLIC_DIR = path.join(__dirname, 'projects');
                 console.error(errName);
                 throw errName;
             }
-        })
+        });
     }
 })();
 
